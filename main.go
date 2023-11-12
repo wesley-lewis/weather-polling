6c3d695f26ec570252f08e83f5f840a49412b2bd
wesley-lewis <lewis.wes.23@gmail.com>
2023-11-12 14:02:04 +0530

    WPoller created along with start()

--- main.go
+++ main.go
@@ -3,11 +3,17 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+	"time"
 )
 
 const (
-	endpoint = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m"
+	endpoint = "https://api.open-meteo.com/v1/forecast"
+)
+
+var (
+	pollInterval = time.Second * 5
 )
 
 type Data struct {
@@ -15,22 +21,46 @@ type Data struct {
 	Hourly    map[string]any `json:"hourly"`
 }
 
-func main() {
+type WPoller struct {
+}
 
+func NewWPoller() *WPoller {
+	return &WPoller{}
+}
+
+func (wp *WPoller) start() {
+	ticker := time.NewTicker(pollInterval)
+	for {
+		data, err := getWeatherResults(52.52, 13.41)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(data)
+		<-ticker.C
+	}
+}
+
+func main() {
+	wp := NewWPoller()
+	wp.start()
 }
 
 func getWeatherResults(lat, long float64) (*Data, error) {
 
-	resp, err := http.Get(endpoint)
+	uri := fmt.Sprintf("%s?latitude=%.2f&longitude=%.2f&hourly=temperature_2m", endpoint, lat, long)
+
+	fmt.Println("------------------------------------------")
+	fmt.Println(uri)
+	resp, err := http.Get(uri)
 	if err != nil {
 		return nil, err
 	}
-
+	defer resp.Body.Close()
 	var data Data
 	if err := json.NewDecoder(resp.Body).Decode(&data); err != nil {
 		return nil, err
 	}
 
-	fmt.Println(data)
 	return &data, nil
 }
